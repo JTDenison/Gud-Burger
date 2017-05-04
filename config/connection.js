ce1d57a0a185c341ab2825ba9889e79fb06e4e01
JTDenison <joshd_84@ymail.com>
2017-05-03 23:24:51 -0400

    commitFinal

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3000,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db",
+  port:3336  // not default sql port!!
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("This happened: " + err);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('gud cheese burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('gud bacon burger');
